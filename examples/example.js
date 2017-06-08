c341adf98cc8c1dffab4cf3e127d4a90c7b66ecd
TobiLG <tobilg@gmail.com>
2017-06-08 18:50:39 +0200

    Updated example

--- examples/example.js
+++ examples/example.js
@@ -1,16 +1,25 @@
 "use strict";
 
-// Use the MesosOperatorApiEventsClient
-const MesosOperatorApiEventsClient = require("../index");
+// Use the MesosOperatorApiClient
+const MesosOperatorApiClient = require("../index");
 
-// Create MesosOperatorApiEventsClient instance
-const eventsClient = new MesosOperatorApiEventsClient({
+// Create MesosOperatorApiClient instance
+const eventsClient = new MesosOperatorApiClient({
     masterHost: "172.17.11.102"
 });
 
 // Wait for "subscribed" event
 eventsClient.on("subscribed", function () {
     console.log("Subscribed to the Mesos Operator API events!");
+    // Call GET_AGENTS
+    eventsClient.getAgents(function (err, data) {
+        console.log("Got result for GET_AGENTS");
+        console.log(JSON.stringify(data));
+    });
+    // Do a reconcile after 3000ms. Demo!
+    setTimeout(function () {
+        eventsClient.reconcile();
+    }, 3000);
 });
 
 // Wait for "unsubscribed" event
@@ -22,6 +31,12 @@ eventsClient.on("unsubscribed", function () {
 eventsClient.on("error", function (errorObj) {
     console.log("Got an error");
     console.log(JSON.stringify(errorObj));
+});
+
+// Wait for "reconciled" event
+eventsClient.on("reconciled", function (stateObj) {
+    console.log("Got reconcile");
+    console.log(JSON.stringify(stateObj));
 });
 
 // Log SUBSCRIBED event
@@ -39,6 +54,18 @@ eventsClient.on("TASK_ADDED", function (eventObj) {
 // Log TASK_UPDATED event
 eventsClient.on("TASK_UPDATED", function (eventObj) {
     console.log("Got TASK_UPDATED");
+    console.log(JSON.stringify(eventObj));
+});
+
+// Log AGENT_ADDED event
+eventsClient.on("AGENT_ADDED", function (eventObj) {
+    console.log("Got AGENT_ADDED");
+    console.log(JSON.stringify(eventObj));
+});
+
+// Log AGENT_REMOVED event
+eventsClient.on("AGENT_REMOVED", function (eventObj) {
+    console.log("Got AGENT_REMOVED");
     console.log(JSON.stringify(eventObj));
 });
 
